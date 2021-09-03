f6c56fd6e44adc321c899a4c3fc2170bd9e4eed5
Diego França <diegobmfranca@gmail.com>
2021-09-03 18:01:53 -0300

    Criando novas páginas para usuário

--- src/components/Usuarios/EditarUsuario.js
+++ src/components/Usuarios/EditarUsuario.js
@@ -1,1 +1,62 @@
+import { React, useState } from 'react';
+//import axios from 'axios';
 
+function EditaUsuario() {
+  //const [nome, setNome] = useState('');
+  //const [sobrenome, setSobrenome] = useState('');
+  //const [senha, setSenha] = useState('');
+  //const [id, setId] = useState('');
+
+  /*const atualizarUsuario = (id) => {
+      axios.put('/usuarios/editar'), {
+          nome,
+          sobrenome,
+          senha
+      }
+  }*/
+  return (
+    <form onSubmit={atualizarUsuario}>
+      <div className="form-group">
+        <label>Nome:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="nome"
+          placeholder="Digite o novo nome"
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <h3>{nome}</h3>
+      </div>
+      <div className="form-group">
+        <label>Sobrenome:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="sobrenome"
+          placeholder="Digite o novo sobrenome"
+          onChange={(e) => setSobrenome(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Senha:</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="Digite a nova senha"
+          onChange={(e) => setSenha(e.target.value)} />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-primary mb-2"
+      // component={Link} to="/test-page"
+      >
+        Enviar
+      </button>
+
+    </form>
+  )
+
+}
+
+export default EditaUsuario
--- src/components/Usuarios/ExibirUsuario.js
+++ src/components/Usuarios/ExibirUsuario.js
@@ -1,14 +1,25 @@
 import { React, useState } from 'react';
 import axios from 'axios';
 
-function exibeUsuarios() {
-  const [listaUsuarios, setListaUsuarios] = useState([]);
-  useEffect(() => {
-    axios.get('/usuarios/listar') // verificar endereço que contém método findAll
-      .then((response) => {
-        setListaUsuarios(response.data)
-      })
-  }, [])
+function ExibeUsuarios() {
+  // const [listaUsuarios, setListaUsuarios] = useState([]);
+  // useEffect(() => {
+  //   axios.get('/usuarios/listar') // verificar endereço que contém método findAll
+  //     .then((response) => {
+  //       setListaUsuarios(response.data)
+  //     })
+  // }, [])
+  // const onDelete = (id) => {
+  //  axios.delete('/usuarios/apagar')
+  //  .then(() => { atualizaTabela(); })
+  //}
+
+  //const atualizaTabela = () => {
+  //axios.get('/usuarios/listar')
+  //.then((atualizaTabela) => {
+  //setListaUsuarios(atualizaTabela.data)
+  //})
+  //}
 
   return (
     <div>
@@ -18,19 +29,22 @@ function exibeUsuarios() {
           <th>Sobrenome</th>
           <th>E-mail</th>
         </tr>
-        {
+        {/* {
           listaUsuarios.map((data) => {
             return (
                             <tr>
                                 <td>{data.nome}</td>
                                 <td>{data.sobrenome}</td>
                                 <td>{data.email}</td>
+                                <td><button onClick={() => setUsuario(data)}>Editar</button></td>
+                                <td><button onClick={() => onDelete(data.id)}>Apagar</button></td>
                             </tr>
-                            <button>Editar</button>
             )
           })
-        }
+        } */}
       </form>
     </div>
   )
 }
+
+export default ExibeUsuarios
