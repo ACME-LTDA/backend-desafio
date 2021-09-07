857c42dcaaf2899596606baa23a88af7a71a9c28
Gustavo <gustavoprocopio01@gmail.com>
2021-09-07 01:12:30 -0300

    Terminando as telas de usuario

--- src/components/Usuarios/ExibirUsuario.js
+++ src/components/Usuarios/ExibirUsuario.js
@@ -1,29 +1,30 @@
-import { React, useEffect, useState } from 'react';
-import axios from 'axios';
-import './ExibirUsuario.css';
-import TextField from '@material-ui/core/TextField';
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import "./ExibirUsuario.css";
+import TextField from "@material-ui/core/TextField";
 
 function ExibirUsuario({ sessao, setSessao }) {
-  const [dados, setDados] = useState({})
-  const [desabilitado, setDesabilitado] = useState(true)
-  const [nome, setNome] = useState('')
-  const [sobrenome, setSobrenome] = useState('')
-  const [habilitaSalvar, setHabilitaSalvar] = useState(false)
+  const [dados, setDados] = useState({});
+  const [desabilitado, setDesabilitado] = useState(true);
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [habilitaSalvar, setHabilitaSalvar] = useState(false);
 
   // TODO usar o refresh token para esta operacao
   useEffect(() => {
     const fetchDados = async () => {
-      const resultado = await axios.get(`/usuarios/${sessao.idUsuario}`, {
-        headers: {
-          'Authorization': `Bearer ${sessao.token}`
-        }
-      })
-        .then(res => res.data.dados)
+      const resultado = await axios
+        .get(`/usuarios/${sessao.idUsuario}`, {
+          headers: {
+            Authorization: `Bearer ${sessao.token}`,
+          },
+        })
+        .then((res) => res.data.dados);
 
-      resultado.sobrenome = resultado.sobrenome ?? ''
-      setDados(resultado)
-      setNome(resultado.nome)
-      setSobrenome(resultado.sobrenome)
+      resultado.sobrenome = resultado.sobrenome ?? "";
+      setDados(resultado);
+      setNome(resultado.nome);
+      setSobrenome(resultado.sobrenome);
     };
 
     fetchDados();
@@ -31,85 +32,95 @@ function ExibirUsuario({ sessao, setSessao }) {
 
   const deletaConta = async () => {
     // TODO colocar uma caixa de confirmacao de deletar a conta
-    const resultado = await axios.delete(`/usuarios/${sessao.idUsuario}/delete`, {
-      headers: {
-        'Authorization': `Bearer ${sessao.token}`
-      }
-    })
-      .then(res => res.data)
-      .catch(err => {
-        console.log('Erro ao remover o usuário: ' + err)
+    const resultado = await axios
+      .delete(`/usuarios/${sessao.idUsuario}/delete`, {
+        headers: {
+          Authorization: `Bearer ${sessao.token}`,
+        },
       })
+      .then((res) => res.data)
+      .catch((err) => {
+        console.log("Erro ao remover o usuário: " + err);
+      });
     if (resultado != null) {
-      console.log('Usuário removido com sucesso! Bye bye ' + sessao.idUsuario)
-      setSessao(null, false, null, false)
+      console.log("Usuário removido com sucesso! Bye bye " + sessao.idUsuario);
+      setSessao(null, false, null, false);
     }
-  }
+  };
 
   const habilitaBotao = () => {
-    if (nome === dados.nome.toString && sobrenome === dados.sobrenome.toString()) {
-      console.log('Desabilita salvar')
-      setHabilitaSalvar(false)
+    if (
+      nome === dados.nome.toString &&
+      sobrenome === dados.sobrenome.toString()
+    ) {
+      console.log("Desabilita salvar");
+      setHabilitaSalvar(false);
+    } else {
+      console.log("Habilita salvar");
+      setHabilitaSalvar(true);
     }
-    else {
-      console.log('Habilita salvar')
-      setHabilitaSalvar(true)
-    }
-  }
+  };
 
   const mudaNome = (event) => {
-    setNome(event.target.value)
-    habilitaBotao()
-  }
+    setNome(event.target.value);
+    habilitaBotao();
+  };
 
   const mudaSobrenome = (event) => {
-    setSobrenome(event.target.value)
-    habilitaBotao()
-  }
+    setSobrenome(event.target.value);
+    habilitaBotao();
+  };
 
   return (
     <div>
-      <div>
-        <form id="form-profile">
-          <div className='form-group'>
-            <label>Nome: </label>
-            <input
-              type="text"
-              value={nome}
-              className="form-control"
-              disabled={desabilitado}
-              onChange={e => mudaNome(e)}
-            />
-          </div>
-          <div className='form-group'>
-            <label>Sobrenome: </label>
-            <input
-              type="text"
-              value={sobrenome}
-              className="form-control"
-              disabled={desabilitado}
-              onChange={e => mudaSobrenome(e)}
-            />
-          </div>
-          <div className='form-group'>
-            <label>E-mail: </label>
-            <input
+      <h2>Meu Cadastro</h2>
+      <div className="container-login3">
+      <form />
+        <div className="row center-form">
+          <div className="form-group col">
+            <div className="form-floating form-spacing">
+              <TextField
+                label="Nome:"
+                type="text"
+                value={nome}
+                className="form-control transparent-input"
+                id="nome"
+                disabled={desabilitado}
+                onChange={(e) => mudaNome(e)}
+                style={{ width: "80%", marginBottom: "20px" }}
+              />
+            </div>
+
+            <div className="form-floating form-spacing">
+              <TextField
+                label="Sobrenome:"
+                type="text"
+                value={sobrenome}
+                className="form-control transparent-input"
+                id="sobrenome"
+                disabled={desabilitado}
+                onChange={(e) => mudaSobrenome(e)}
+                style={{ width: "80%", marginBottom: "20px" }}
+              />
+            </div>
+        
+
+          <div className="form-floating form-spacing">
+            <TextField
+              label=""
               type="email"
               value={dados.email}
-              className="form-control"
+              className="form-control transparent-input"
+              id="email"
               disabled
+              style={{ width: "80%", marginBottom: "-11px" }}
             />
           </div>
-        </form>
-        <div id="wrapper">
+          </div>
+        </div>
 
-          <button
-            type="button"
-            className="btn btn-danger mb-2"
-            onClick={async () => deletaConta()}
-          >
-            Deletar Conta
-          </button>
+        <div id="wrapper">
+        
           {desabilitado ?
             <button
               type="button"
@@ -126,9 +137,17 @@ function ExibirUsuario({ sessao, setSessao }) {
             >
               Salvar alterações
             </button>}
+            <button
+            type="button"
+            className="btn btn-danger mb-2"
+            onClick={async () => deletaConta()}
+          >
+            Deletar Conta
+          </button>
         </div>
-      </div >
-    </div >
-    );
-}  
+      </div>
+    </div>
+  );
+}
+
 export default ExibirUsuario;
