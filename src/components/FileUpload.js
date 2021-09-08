78413f5ec6996d65546682546cd4a69681ae0397
Diego França <diegobmfranca@gmail.com>
2021-09-08 12:03:11 -0300

    Renomeando arquivo FileUpload.js

---
+++ src/components/FileUpload.js
@@ -0,0 +1,28 @@
+
+import React, { useRef } from 'react';
+import axios from 'axios';
+
+function FileUpload() {
+  const filesElement = useRef(null);
+
+  const sendFile = async () => {
+    const dataForm = new FormData();
+    for (const file of filesElement.current.files) {
+      dataForm.append('file', file);
+    }
+    const res = await fetch(`http://localhost:3001/uploads`, {
+      method: 'POST',
+      body: dataForm,
+    });
+    const data = await res.parse;
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <input type="file" multiple ref={filesElement} />
+      <button onClick={sendFile}>Send file</button>
+    </div>
+  );
+}
+export default FileUpload;
